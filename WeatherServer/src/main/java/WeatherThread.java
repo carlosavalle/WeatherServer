4968e75ae5b85ac5b840d8918bf5dd07d9884fde
carlosavalle <carlosavalle@users.noreply.github.com>
2021-03-29 22:20:59 -0300

    working

--- WeatherServer/src/main/java/WeatherThread.java
+++ WeatherServer/src/main/java/WeatherThread.java
@@ -26,8 +26,7 @@ public class WeatherThread implements Runnable  {
 
 
         try {
-            // location = br.readLine();
-            //  location ="Japon";
+
             String result = readHTTP("https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=imperial&apiKey=6ae2281a443225f45f30cc3a4a1d37b2");
             WeatherCondition wc = mapper.readValue(result, WeatherCondition.class);
             //  WeatherForecast wc = mapper.readValue(result, WeatherForecast.class);;
